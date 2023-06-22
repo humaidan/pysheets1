911fe43a3f4e19b0b9e47817536dd99ab8cddc71
AMH <humaidan@gmail.com>
2023-06-22 21:06:20 +0300

    init

---
+++ config.py
@@ -0,0 +1,10 @@
+CONFIG = {
+    'sheetname_source': 10_000,
+    'sheetname_dest': 5000,
+    'last_date': '2022-10-31 23:59:00',
+    'field_mapping': {
+        "Field1": {"row": 1, "col": 1},
+        "Field2": {"row": 2, "col": 2},
+        "Field3": {"row": 3, "col": 3},
+    }
+}
